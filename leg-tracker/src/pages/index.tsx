aee484e0df7d2c261cd7e4d25d502754b3f52fcf
emdat <datta.emon@gmail.com>
2023-05-02 21:38:52 -0700

    Connect to REST API backend instead of using mock data

--- leg-tracker/src/pages/index.tsx
+++ leg-tracker/src/pages/index.tsx
@@ -2,19 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { GridOptions } from '@ag-grid-community/core';
-import MOCK_DATA from '../components/MOCK_DATA.json';
 import CustomTooltip from '../components/Tooltip.tsx';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
  const [columnDefs] = useState([
-    { headerName: 'Bill Number', field: 'bill_num'},
+    { headerName: 'Bill Number', field: 'bill_number'},
     { field: 'name' },
     { field: 'author' },
     { field: 'notes', editable: true },
@@ -25,7 +24,15 @@ export default function Home() {
     defaultColDef: { tooltipValueGetter: (params) => params.value}
   };
 
-  const rowData = useMemo(() => MOCK_DATA, []);
+  const [rowData, setData] = useState(null);
+  useEffect(() => {
+      async function fetchData() {
+        const response = await fetch('http://127.0.0.1:5000/legislation-tracker');
+        const rowData = await response.json();
+        setData(rowData);
+      }
+      fetchData();
+  }, [])
   
   return (
     <div 
