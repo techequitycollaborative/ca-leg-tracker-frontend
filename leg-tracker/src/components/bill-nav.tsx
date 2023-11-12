8b9f737e69df676f667dbeb685a33d2885136e0a
Matt Brooks <matt@techequitycollaborative.org>
2023-11-11 16:58:26 -0800

    added search/dashboard page components, sample bill data

---
+++ leg-tracker/src/components/bill-nav.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next';
+
+interface Props {
+}
+
+const BillNav: NextPage<Props> = function BillNav(props) {
+  return (
+    <div className='flex border-2 border-black p-4 m-4'>
+      <div className='flex-1 w-1/2'>
+        <p>bill filters</p>
+      </div>
+      <div className='flex-1 w-1/2 text-right'>
+        <p>bill sort</p>
+      </div>
+    </div>
+  );
+};
+
+export default BillNav;
--- leg-tracker/src/components/footer.tsx
+++ leg-tracker/src/components/footer.tsx
@@ -5,7 +5,11 @@ interface Props {
 
 const Footer: NextPage<Props> = function Footer(props) {
   return (
-    <></>
+    <>
+      <div className="w-full bg-gray-300">
+        <p>footer content</p>
+      </div>
+    </>
   );
 };
 
