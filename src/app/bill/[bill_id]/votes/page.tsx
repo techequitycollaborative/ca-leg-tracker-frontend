6757c2641d41e04e012b699c7fde802412671a87
Matt Brooks <matt@techequitycollaborative.org>
2024-02-14 15:47:50 -0800

    added vote history

--- src/app/bill/[bill_id]/votes/page.tsx
+++ src/app/bill/[bill_id]/votes/page.tsx
@@ -1,47 +1,48 @@
 import { repositories } from '@/repositories/index';
 import { PageProps } from '@/definitions/page.types.definitions';
+import { ChamberVoteResult } from "@/infrastructure/drizzle/schema/chamber-vote-result";
 import BillNav from '@/components/bill-nav';
 import VoteListItem from '@/components/vote-list-item';
 
 const Page = async ({ params }: PageProps) => {
   const { bill_id } = params;
   const bill = await repositories.billRepository.getById(bill_id);
+  const votes = await repositories.billRepository.getBillVotes(parseInt(bill_id));
+
+  function isCommitteeVote(vote: ChamberVoteResult) {
+    const votesFor = (vote.votesFor == null ? 0 : vote.votesFor);
+    const votesAgainst = (vote.votesAgainst == null ? 0 : vote.votesAgainst);
+    const votesOther = (vote.votesOther == null ? 0 : vote.votesOther);
+
+    if (votesFor + votesAgainst + votesOther < 25) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
   return (
     <>
       <BillNav
         current="votes"
       />
       <div>
-        <VoteListItem
-          date="[date]"
-          result="[result]"
-          location="[location]"
-          ayes={10}
-          noes={10}
-          nvr={0}
-          motion="[motion]"
-          borderBottom={true}
-        />
-        <VoteListItem
-          date="[date]"
-          result="[result]"
-          location="[location]"
-          ayes={10}
-          noes={10}
-          nvr={0}
-          motion="[motion]"
-          borderBottom={true}
-        />
-        <VoteListItem
-          date="[date]"
-          result="[result]"
-          location="[location]"
-          ayes={10}
-          noes={10}
-          nvr={0}
-          motion="[motion]"
-          borderBottom={false}
-        />
+      {votes &&
+        votes.map((x: any, i: any) => (
+          <VoteListItem
+            key={i}
+            date={new Date(x.vote.voteDate + 'T00:00:00').toLocaleString([], { year: 'numeric', month: 'long', day: 'numeric' })}
+            result={x.vote.voteResult}
+            location={x.chamber.name + (isCommitteeVote(x.vote) ? " (Committee)" : " (Full Chamber)")}
+            threshold={x.vote.voteThreshold}
+            ayes={x.vote.votesFor}
+            noes={x.vote.votesAgainst}
+            nvr={x.vote.votesOther}
+            motion={x.vote.voteText}
+            borderBottom={i == votes.length - 1 ? false : true}
+          />
+      ))}
       </div>
     </>
   );
--- src/components/vote-list-item.tsx
+++ src/components/vote-list-item.tsx
@@ -2,12 +2,13 @@ import { NextPage } from 'next';
 
 interface Props {
   date: string;
-  result: string;
   location: string;
+  motion: string;
+  threshold: string;
+  result: string;
   ayes: number;
   noes: number;
   nvr: number;
-  motion: string;
   borderBottom: boolean;
 }
 
@@ -15,12 +16,13 @@ const VoteListItem: NextPage<Props> = function VoteListItem(props) {
   return (
     <div className={"m-4" + (props.borderBottom ? " border-b border-gray-500 pb-4" : "")}>
       <p><span className="font-bold">Date:</span> {props.date}</p>
-      <p><span className="font-bold">Result:</span> {props.result}</p>
       <p><span className="font-bold">Location:</span> {props.location}</p>
+      <p><span className="font-bold">Motion:</span> {props.motion}</p>
+      <p><span className="font-bold">Threshold:</span> {props.threshold}</p>
+      <p className="capitalize"><span className="font-bold">Result:</span> {props.result}</p>
       <p><span className="font-bold">Ayes:</span> {props.ayes}</p>
       <p><span className="font-bold">Noes:</span> {props.noes}</p>
       <p><span className="font-bold">NVR:</span> {props.nvr}</p>
-      <p><span className="font-bold">Motion:</span> {props.motion}</p>
     </div>
   );
 };
